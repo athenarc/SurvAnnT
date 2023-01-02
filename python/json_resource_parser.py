fdf1a92c88297c0904441fd6829a45aae8d4113d
tzerefos <tzerefos@athenarc.gr>
2023-01-02 13:05:45 +0200

    Updated invitation message

--- python/json_resource_parser.py
+++ python/json_resource_parser.py
@@ -1,5 +1,6 @@
-import json, os, sys, pandas as pd
+import json, os, sys, pandas as pd, pymysql
 from sqlalchemy import create_engine
+from urllib.parse import quote_plus
 
 if len(sys.argv) < 9 :
 	print("Not enough arguments given")
@@ -48,9 +49,7 @@ if num_of_articles != -1:
 	else:
 		dataframe = dataframe[:num_of_articles]
 
-engine = create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}".format(host=hostname, db=dbname, user=uname, pw=pwd))
-
-print(dataframe)
+engine = create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}".format(host=hostname, db=dbname, user=uname, pw=quote_plus(pwd)))
 
 try:
 	# bug when zipped file contains articles in json format
